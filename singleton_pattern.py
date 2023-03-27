7af1d0e01ef965cf6e0c8fb38db11e243708038e
Rubyrubylai <r844312@gmail.com>
2023-03-27 13:01:44 +0800

    Practice singleton pattern when multithread

--- singleton_pattern.py
+++ singleton_pattern.py
@@ -1,7 +1,9 @@
+from threading import Lock, Thread
+
+
 class ChocolateBoiler:
     __uniqueInstance = None
 
-    # 創建類實例的靜態方法
     def __new__(cls):
         if not cls.__uniqueInstance:
             cls.__uniqueInstance = super().__new__(cls)
@@ -12,3 +14,30 @@ class ChocolateBoiler:
 s1 = ChocolateBoiler()
 s2 = ChocolateBoiler()
 print(s1 is s2)
+
+
+# thread-safe
+class MultiChocolateBoiler:
+    __uniqueInstance = None
+    __lock = Lock()
+    value = None
+
+    def __new__(cls, value):
+        if not cls.__uniqueInstance:
+            with cls.__lock:
+                if not cls.__uniqueInstance:
+                    cls.__uniqueInstance = super().__new__(cls)
+                    cls.__uniqueInstance.value = value
+
+        return cls.__uniqueInstance
+
+
+def test_singleton(value):
+    s = MultiChocolateBoiler(value)
+    print(s.value)
+
+
+process1 = Thread(target=test_singleton, args=('toy',))
+process2 = Thread(target=test_singleton, args=('store',))
+process1.start()
+process2.start()
